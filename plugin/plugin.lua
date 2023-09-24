07d7c29869153c51b745c1f8133b2ab46af62886
esakyoar <yohib.hussain@gmail.com>
2023-09-24 19:22:52 +0530

    Added 30 different quotes

---
+++ plugin/plugin.lua
@@ -0,0 +1,38 @@
+local quotes = {
+
+    "Why do Vim users make good detectives? Because they're always in search mode.",
+    "Vim users don't need therapy; they have escape keys.",
+    "The first rule of Vim club: You do not talk about Vim club.",
+    "What do you call a Vim user who meditates? A zen-coder.",
+    "Vim is like a fine wine; it gets better with age.",
+    "My relationship with Vim is like a rollercoaster. I yank, I put, I quit.",
+    "Vim is the only editor where you need a cheat sheet for the cheat sheet.",
+    "I started using Vim, and now I can quit anytime I want. I just don't want to.",
+    "Vim is the editor of choice for people who love typing more than they love their sanity.",
+    "Why did the Vim user go broke? He couldn't save enough.",
+    "Vim is like a Swiss Army knife: it has a tool for everything, and you only need one.",
+    "Vim users don't need a mouse; they have ninja-like cursor skills.",
+    "When Vim users die, they don't rest in peace; they exit without saving.",
+    "Vim is the editor that makes you realize that learning a new language is easier.",
+    "What do Vim users say when they win a game? ':wq! I win!'",
+    "Vim users don't believe in heaven and hell; they believe in insert mode and command mode.",
+    "What's a Vim user's favorite beverage? 'Esc'presso.",
+    "Vim users are like programmers in the Matrix; they see the code.",
+    "Vim users never forget their first time they quit Vim without saving.",
+    "If Vim were a movie, it would be titled 'The Neverending Edits.'",
+    "What do you call a Vim user who loves nature? An 'Outdoors-man' mode.",
+    "Vim is the reason some programmers develop carpal tunnel syndrome.",
+    "Vim users are good at multitasking: they edit code, swap windows, and remap keys simultaneously.",
+    "Vim users are the master of shortcuts. Why use three keys when you can use one?",
+    "Vim users don't make typos; they create new syntax.",
+    "What do Vim users and guitarists have in common? They both play in chords.",
+    "Why don't Vim users like going to the beach? They can't stand being in INSERT mode.",
+    "Vim users have a special relationship with their brackets. They never let them go.",
+    "When in doubt, Vim it out.",
+    "Vim users are like poets; they write their code one word at a time.",
+}
+
+local rand_number = math.random(1,30)
+
+print(quotes[rand_number])
+
